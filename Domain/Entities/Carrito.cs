e569425883a20f5591df482bea0c14f882ca150c
Leonardo Duca <Leonardo Duca>
2022-09-16 18:55:06 -0300

    Agregar archivos de proyecto.

---
+++ Domain/Entities/Carrito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Carrito
+    {
+        public int CarritoId { get; set; }
+        public int ClienteId { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ Domain/Entities/CarritoProducto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CarritoProducto
+    {
+        public int CarritoId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
