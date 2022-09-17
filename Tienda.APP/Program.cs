669da8ec350b7089cab57363900ecf8176a40ef6
Leonardo Duca <Leonardo Duca>
2022-09-16 22:08:25 -0300

    Agrego aplicacion de consola con archivo de configuracion

---
+++ Tienda.APP/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tienda.APP;
+
+//1. Create a service collection for DI
+var serviceCollection = new ServiceCollection();
+
+//2. Build a configuration https://www.youtube.com/watch?v=tQdNlju2UXo
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+Console.WriteLine(configuration);
+
+//3. Add the configuration to the service collection.
+serviceCollection.AddSingleton<IConfiguration>(configuration);
+serviceCollection.AddSingleton<Test>();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var testIntance = serviceProvider.GetService<Test>();
+testIntance.TestMethord();
